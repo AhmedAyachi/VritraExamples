db033a8a31c66ef0a5ae76361f937f63e0105a96
AhmedAyachi <aayachi032@gmail.com>
2023-11-18 23:05:54 +0100

    cleanup store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,25 +1,5 @@
 
 
 export const store={
-    langname:localStorage.getItem("langname")||"english",
-    user:{
-        firstname:"ahmed",
-        lastname:"ayachi",
-        age:22,
-        arrays:[
-            [[1],[2],[3]],
-            [[1],[2],[3]],
-        ],
-        accounts:[
-            {site:"facebook",username:"aayachi032@gmail.com"},
-            {site:"gmail",username:"dofus592@gmail.com"},
-            {attributs:["a","b",[1,2,3]]},
-        ],
-        balance:{
-            total:1500,
-            date:{
-                day:7,
-            },
-        },
-    },
+    
 };
